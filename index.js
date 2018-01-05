0a807d74c5c556ed33885e38581856870eddeba7
Nathan Nobert <nobert@ualberta.ca>
2018-01-05 11:12:49 -0700

    updating index.js to have header documentation and file pathing fixed

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+/**
+AUCSC320 Camrose Boys & Girls Club App.
+Started January 4th, 2018.
+Group:
+  Nathan Nobert - Team Leader, Programmer, Tester
+  Fatima Bin Sumait - Programmer, Tester
+  Jack Shea - Programmer, Tester
+  Katharina Reddecop - Programmer, Tester
+
+This is our main Javascript file used for the starting point of our app.
+The framework we are using to run our Desktop/Web App on is Electron, this is a packaging
+framework that uses HTML, CSS, Javascript, and Node.js to build and applications.
+
+Methods/Functions:
+  createWindow()
+    -This creates our main application window
+
+
+Known Bugs:
+  -
+*/
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
@@ -10,7 +31,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'src/index.html'),
     protocol: 'file:',
     slashes: true
   }))
