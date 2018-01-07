e4fb485a7e8a0c1278c71c9882d44e3bf7670d4c
Nathan Nobert <nobert@ualberta.ca>
2018-01-06 17:03:21 -0700

    added new HTML/js files for home page of app | attempted to get functionality of signing in to work

--- src/index.js
+++ src/index.js
@@ -26,6 +26,9 @@ const url = require('url')
 
 let win
 
+/*
+This function starts the first window for the user to see the interface
+*/
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({show: false, width: 800, height: 600});
@@ -53,7 +56,50 @@ function createWindow () {
     // when you should delete the corresponding element.
     win = null
   })
-}
+}//createWindow
+
+
+
+// const notifyBtn = document.getElementById('signInSubmit')
+// notifyBtn.addEventListener('click', function (event) {
+//       const modalPath = path.join('file://', __dirname, 'appHomePage.html')
+//       let win = new BrowserWindow({ width: 400, height: 200 })
+//       win.on('close', function () { win = null })
+//       win.loadURL(modalPath)
+//       win.show()
+//     })
+
+
+var attempt = 3; // Variable to count number of attempts.
+/*
+This function runs when the submit button to login is clicked. 
+This checks the user entered username and password against the hard coded in info *currently*
+*/
+function checkLogin(){
+  var username = document.getElementById("signInUsername").value;
+  var password = document.getElementById("signInPassword").value;
+
+
+  //hard coding in the usernames and password
+  if ( username == "admin" && password == "password"){
+    alert ("Login successfully");
+    window.Location = "appHomePage.html"; //Brings the user to next page
+    return true;
+  }
+  else{
+    attempt --;// Decrementing by one.
+    alert("You have left "+attempt+" attempt;");
+    // Disabling fields after 3 attempts.
+    if( attempt == 0){
+      document.getElementById("signInUsername").disabled = true;
+      document.getElementById("signInPassword").disabled = true;
+      document.getElementById("signInSubmit").disabled = true;
+      return false;
+    }//if
+  }//else
+}//checkLogin
+
+
 
 app.on('ready', createWindow)
 
@@ -61,10 +107,10 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+})//app.on
 
 app.on('activate', () => {
   if (win === null) {
     createWindow()
   }
-})
+}) //app.on
