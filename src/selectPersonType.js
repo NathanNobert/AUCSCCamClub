a338c0e1b3a1b025e888882e2c783a3acf6461a9
Nathan Nobert <nobert@ualberta.ca>
2018-01-08 15:12:44 -0700

    fixed error where button diaplayed word 'undefined' for a short period of time

--- src/selectPersonType.js
+++ src/selectPersonType.js
@@ -1,16 +1,9 @@
-/*
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
-*/
-
-
 function openNextPage(idOfButton) {
 	if(idOfButton === 'Donors'){
-    	document.getElementById(idOfButton).innerHTML = window.location.replace("donorForm.html");
+    	document.getElementById(Donors).innerHTML = window.location.replace("donorForm.html");
 	}else if(idOfButton === 'volunteer'){
-		document.getElementById(idOfButton).innerHTML = window.location.replace("volunteerForm.html")
+		document.getElementById(volunteer).innerHTML = window.location.replace("volunteerForm.html")
 	}else{
-		document.getElementById(idOfbutton).innterHTML = window.location.replace("memberForm.html")
+		document.getElementById(member).innterHTML = window.location.replace("memberForm.html")
 	}
 }
