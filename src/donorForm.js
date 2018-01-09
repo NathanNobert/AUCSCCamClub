0d368487b8f2ebec383ef8f598d98e59ed72b4d0
Nathan Nobert <nobert@ualberta.ca>
2018-01-09 16:17:46 -0700

    fixed formatting on how the text document was representing information from the donor Form

--- src/donorForm.js
+++ src/donorForm.js
@@ -29,10 +29,12 @@ function infoStorage() {
 	var fs = require('fs');
 	var stream = fs.createWriteStream("donorFormEntries/" + donorFormInfo[0].last + ", " + donorFormInfo[0].first + ".txt");
 	stream.once('open', function(fd) {
-	stream.write("Full Name: " + donorFormInfo[0].first + " " + donorFormInfo[0].last + " Contact Name: " + donorFormInfo[0].contact + "\r\n");
-	stream.write("Email Address: " + donorFormInfo[0].email + " Phone Number: " + donorFormInfo[0].phone + "\r\n");
+	stream.write("Full Name: " + donorFormInfo[0].first + " " + donorFormInfo[0].last + "\r\n");
+	stream.write("Contact Name: " + donorFormInfo[0].contact + "\r\n");
+	stream.write("Email Address: " + donorFormInfo[0].email + "\r\n");
+	stream.write("Phone Number: " + donorFormInfo[0].phone + "\r\n");
 	stream.write("Address: " + donorFormInfo[0].addressInfo + " , " + donorFormInfo[0].cityInfo + " , " + donorFormInfo[0].postal + "\r\n");
-	stream.write("Monetary amount donated: $" + donorFormInfo[0].monetary + "\n");
+	stream.write("Monetary amount donated: $" + donorFormInfo[0].monetary + "\r\n");
 	stream.write("Non-Monetary estimated value: $" + donorFormInfo[0].nonMonetary + " The item donated: " + donorFormInfo[0].item + "\r\n" + "\r\n");
 	stream.write("Comments: " + donorFormInfo[0].comment);
 	stream.end();
