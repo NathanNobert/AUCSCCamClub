15e5b3aea9b46a564652f2737e28cb52f022046c
JackShea1998 <jshea@ualberta.ca>
2018-01-10 16:59:20 -0700

    Takes the user input from the search bars in the Search existing for donors and puts turns them into the name of a text file that can later be searched

--- src/existingOrNewDonor.js
+++ src/existingOrNewDonor.js
@@ -5,3 +5,10 @@ const url = require('url')
 function moveToDonorForm() {
 	document.getElementById(newdonor).innerHTML = window.location.replace("donorForm.html");
 }
+
+function openFileFromSearch() {
+	var searchedFirstName = document.getElementById("firstname").value;
+	var searchedLastName = document.getElementById("lastname").value;
+	var fileToSearch = searchedLastName + ", " + searchedFirstName + ".txt";
+	document.getElementById(search).innerHTML = window.location.replace("testIfFileNameSaves.html");
+}
