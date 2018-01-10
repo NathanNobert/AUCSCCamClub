5b1cbca8ca29e668ed17b8b15cf88ebfbefa949a
fbinsuma <fbinsuma@ualberta.ca>
2018-01-10 14:44:40 -0700

    Makes sure phone number is correct

--- src/donorForm.js
+++ src/donorForm.js
@@ -6,7 +6,7 @@ const{app, BrowserWindow} = require('electron')
 const path = require ('path')
 const url = require ('url')
 
-function infoStorage() {
+function infoStorage(inputtxt) {
 
 	var donorFormInfo = [
 		{
@@ -25,40 +25,33 @@ function infoStorage() {
 		}
 	];
 
+	var donorForm = /^\d{10}$/;
+	if(inputtxt.value.match(donorForm)){
+		var fs = require('fs');
+		var stream = fs.createWriteStream("donorFormEntries/" + donorFormInfo[0].last + ", " + donorFormInfo[0].first + ".txt");
+		stream.once('open', function(fd) {
+		stream.write("Full Name: " + donorFormInfo[0].first + " " + donorFormInfo[0].last + "\r\n");
+		stream.write("Contact Name: " + donorFormInfo[0].contact + "\r\n");
+		stream.write("Email Address: " + donorFormInfo[0].email + "\r\n");
+		stream.write("Phone Number: " + donorFormInfo[0].phone + "\r\n");
+		stream.write("Address: " + donorFormInfo[0].addressInfo + " , " + donorFormInfo[0].cityInfo + " , " + donorFormInfo[0].postal + "\r\n");
+		stream.write("Monetary amount donated: $" + donorFormInfo[0].monetary + "\r\n");
+		stream.write("Non-Monetary estimated value: $" + donorFormInfo[0].nonMonetary + " The item donated: " + donorFormInfo[0].item + "\r\n" + "\r\n");
+		stream.write("Comments: " + donorFormInfo[0].comment);
+		stream.end();
+		});
+		alert("Your information has been submitted, Thank you.", "Donor Form Submission");
+		gotoMainMenu();
+		return true;
 
-	var fs = require('fs');
-	var stream = fs.createWriteStream("donorFormEntries/" + donorFormInfo[0].last + ", " + donorFormInfo[0].first + ".txt");
-	stream.once('open', function(fd) {
-	stream.write("Full Name: " + donorFormInfo[0].first + " " + donorFormInfo[0].last + "\r\n");
-	stream.write("Contact Name: " + donorFormInfo[0].contact + "\r\n");
-	stream.write("Email Address: " + donorFormInfo[0].email + "\r\n");
-	stream.write("Phone Number: " + donorFormInfo[0].phone + "\r\n");
-	stream.write("Address: " + donorFormInfo[0].addressInfo + " , " + donorFormInfo[0].cityInfo + " , " + donorFormInfo[0].postal + "\r\n");
-	stream.write("Monetary amount donated: $" + donorFormInfo[0].monetary + "\r\n");
-	stream.write("Non-Monetary estimated value: $" + donorFormInfo[0].nonMonetary + " The item donated: " + donorFormInfo[0].item + "\r\n" + "\r\n");
-	stream.write("Comments: " + donorFormInfo[0].comment);
-	stream.end();
-	});
-	alert("Your information has been submitted, Thank you.", "Donor Form Submission");
-	gotoMainMenu();
+	}
+	else{
+		alert("Not a valid Phone Number");
+		return false;
+	}
+
 
 }
-
-		function validatingPhoneNum() {
-			var num , phoneAlert;
-
-			//Gets the value inputed in the phone number input box
-			num = document.getElementById("phoneNumber").value;
-
-			//If num is not a number or length greater than 11 numbers
-			if (isNan(num) || num.length < 10 || num.length > 10) {
-				phoneAlert = "Please try again";
-			}
-			else{
-				phoneAlert = "Input Okay";
-			}
-			document.getElementById().innerHTML = phoneAlert
-		}
 
 /*
 This function sends the user back to the login screen
