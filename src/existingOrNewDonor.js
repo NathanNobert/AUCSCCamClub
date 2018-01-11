4ab9681704899d396321f535d1722206608d8637
JackShea1998 <jshea@ualberta.ca>
2018-01-11 16:53:59 -0700

    made the search button open a local text file. Right now it is a specific one, but it is going to be changed

--- src/existingOrNewDonor.js
+++ src/existingOrNewDonor.js
@@ -1,14 +1,21 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
+var opener = require("opener");
+
+
+var searchedFirstName = document.getElementById("firstname").value;
+var searchedLastName = document.getElementById("lastname").value;
+var fileToSearch = searchedLastName + ", " + searchedFirstName + ".txt";
+//var searchedFile = "../donorFormEntries/" + fileToSearch
+var testThing = "Fudd, Elmer.txt"
+var searchedFile = "../donorFormEntries/Fudd, Elmer.txt"
 
 function moveToDonorForm() {
 	document.getElementById(newdonor).innerHTML = window.location.replace("donorForm.html");
 }
 
-function openFileFromSearch() {
-	var searchedFirstName = document.getElementById("firstname").value;
-	var searchedLastName = document.getElementById("lastname").value;
-	var fileToSearch = searchedLastName + ", " + searchedFirstName + ".txt";
-	document.getElementById(search).innerHTML = window.location.replace("testIfFileNameSaves.html");
+function moveToExisistingForm() {
+	var searchedFile = "../donorFormEntries/Fudd, Elmer.txt"
+	document.getElementById(search).innerHTML = window.location.replace(searchedFile);
 }
