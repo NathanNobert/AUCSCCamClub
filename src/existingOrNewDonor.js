a3297af7488b368ba3c944eba8d316786386a556
Nathan Nobert <nobert@ualberta.ca>
2018-01-11 16:57:53 -0700

    adding predictive text of an array to add a new or select existing donor. Still needs full implementation of a built in array consisting of names"

--- src/existingOrNewDonor.js
+++ src/existingOrNewDonor.js
@@ -5,3 +5,8 @@ const url = require('url')
 function moveToDonorForm() {
 	document.getElementById(newdonor).innerHTML = window.location.replace("donorForm.html");
 }
+
+var input = document.getElementById("predictiveList");
+new Awesomplete(input, {
+  list: ["Ada", "Java", "JavaScript", "Brainfuck", "LOLCODE", "Node.js", "Ruby on Rails"]
+});
