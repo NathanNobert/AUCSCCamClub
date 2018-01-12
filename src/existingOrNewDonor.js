d229a9a29bc67a8a51480c1ed579338014cef4f3
JackShea1998 <jshea@ualberta.ca>
2018-01-12 13:19:55 -0700

    Made the searched text fiel pop up in it's own window

--- src/existingOrNewDonor.js
+++ src/existingOrNewDonor.js
@@ -1,24 +1,31 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
-var opener = require("opener");
-
 
 var searchedFirstName = document.getElementById("firstname").value;
 var searchedLastName = document.getElementById("lastname").value;
 var fileToSearch = searchedLastName + ", " + searchedFirstName + ".txt";
-//var searchedFile = "../donorFormEntries/" + fileToSearch
-var testThing = "Fudd, Elmer.txt"
-var searchedFile = "../donorFormEntries/Fudd, Elmer.txt"
+var searchedFile = "../donorFormEntries/" + fileToSearch
 
 function moveToDonorForm() {
 	document.getElementById(newdonor).innerHTML = window.location.replace("donorForm.html");
 }
 
 function moveToExisistingForm() {
+	const remote = require('electron').remote;
+	const BrowserWindow = remote.BrowserWindow;
 	var searchedFirstName = document.getElementById("firstname").value;
 	var searchedLastName = document.getElementById("lastname").value;
 	var fileToSearch = searchedLastName + ", " + searchedFirstName + ".txt";
 	var searchedFile = "../donorFormEntries/" + fileToSearch
-	document.getElementById(search).innerHTML = window.location.replace(searchedFile);
+	var win = new BrowserWindow({ show: false, width: 800, height: 600 });
+	win.loadURL(url.format({
+    pathname: path.join(__dirname, searchedFile),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  //waiting to show the screen until now allows the screen and elements to load.
+  win.show();
+	
 }
