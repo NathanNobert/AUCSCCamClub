98c56ebf39ee2dc9c5d67ebf7457ebb5e30196e7
KatharinaReddecop <reddecop@ualberta.ca>
2018-01-12 12:06:40 -0700

    Updating my files and removed the confirmation html and js since Nathan found a better way to create a confirmation message.

--- src/donorForm.js
+++ src/donorForm.js
@@ -1,20 +1,15 @@
 /*
 This file contains all the javascript used for the donorForm page.
 */
-
 const{app, BrowserWindow} = require('electron')
 const path = require ('path')
 const url = require ('url')
 const BrowserWindow = electron.remote.BrowserWindow
-
 //const {dialog} = require('electron').remote;
-
-
 /*
 This function reads the information from the donorForm.html file and creates a report/text document
 */
-function infoStorage() {
-
+function infoStorage(inputtxt) {
 	//var fileName = askWhereToSave();
 
 
@@ -33,33 +28,39 @@ function infoStorage() {
 			item: document.getElementById("nonMonetaryItem").value,
 			comment: document.getElementById("commentBox").value
 		}
-	];
+	];//donorFormInfo
 
-	// fs.writeFile(fileName, content, (err) => {
- //        if(err){
- //            alert("An error ocurred creating the file "+ err.message)
- //        }
-                    
- //        alert("The file has been succesfully saved");
- //    });
+	var donorForm = /^\d{10}$/;
+	if(inputtxt.value.match(donorForm)){
+		
+		var fs = require('fs');
+		var stream = fs.createWriteStream("donorFormEntries/" + donorFormInfo[0].last + ", " + donorFormInfo[0].first + ".txt");
+		stream.once('open', function(fd) {
+		stream.write("Full Name: " + donorFormInfo[0].first + " " + donorFormInfo[0].last + "\r\n");
+		stream.write("Contact Name: " + donorFormInfo[0].contact + "\r\n");
+		stream.write("Email Address: " + donorFormInfo[0].email + "\r\n");
+		stream.write("Phone Number: " + donorFormInfo[0].phone + "\r\n");
+		stream.write("Address: " + donorFormInfo[0].addressInfo + " , " + donorFormInfo[0].cityInfo + " , " + donorFormInfo[0].postal + "\r\n");
+		stream.write("Monetary amount donated: $" + donorFormInfo[0].monetary + "\r\n");
+		stream.write("Non-Monetary estimated value: $" + donorFormInfo[0].nonMonetary + " The item donated: " + donorFormInfo[0].item + "\r\n" + "\r\n");
+		stream.write("Comments: " + donorFormInfo[0].comment);
+		stream.end();
+		});
 
-	var fs = require('fs');
-	var stream = fs.createWriteStream("donorFormEntries/" + donorFormInfo[0].last + ", " + donorFormInfo[0].first + ".txt");
-	stream.once('open', function(fd) {
-	stream.write("Full Name: " + donorFormInfo[0].first + " " + donorFormInfo[0].last + "\r\n");
-	stream.write("Contact Name: " + donorFormInfo[0].contact + "\r\n");
-	stream.write("Email Address: " + donorFormInfo[0].email + "\r\n");
-	stream.write("Phone Number: " + donorFormInfo[0].phone + "\r\n");
-	stream.write("Address: " + donorFormInfo[0].addressInfo + " , " + donorFormInfo[0].cityInfo + " , " + donorFormInfo[0].postal + "\r\n");
-	stream.write("Monetary amount donated: $" + donorFormInfo[0].monetary + "\r\n");
-	stream.write("Non-Monetary estimated value: $" + donorFormInfo[0].nonMonetary + " The item donated: " + donorFormInfo[0].item + "\r\n" + "\r\n");
-	stream.write("Comments: " + donorFormInfo[0].comment);
-	stream.end();
-	});
-	alert("Your information has been submitted, Thank you.", "Donor Form Submission");
-	gotoMainMenu();
+		alert("Your information has been submitted, Thank you.", "Donor Form Submission");
+		gotoMainMenu();
 
-}
+
+
+		return true;
+
+	}//if
+	else{
+		alert("Not a valid Phone Number");
+		return false;
+	}//else
+
+}//infoStorage
 
 /*
 Will use this function if we decide to give the user the option to choose where they want to save
@@ -72,21 +73,12 @@ function askWhereToSave(){
 		    return;
 		}
 	});
-}
+}//askWhereToSave
 
-var backBtn = document.getElementById("backButton");
-
-backBtn.addEventListener('click', () => {
-	alert("Testing");
-	document.getElementById(gotoMainMenu).innerHTML = window.location.replace("selectPersonType.html")
-});
-function confirmMessage(){
-	gotoMainMenu()
-}
 
 /*
 This function sends the user back to the login screen
 */
 function gotoMainMenu() {
   document.getElementById(gotoMainMenu).innerHTML = window.location.replace("selectPersonType.html");
-}
+}//gotoMainMenu
