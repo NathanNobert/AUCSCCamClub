cbaff8f368f837586ff9121758c94d33defd70ea
JackShea1998 <jshea@ualberta.ca>
2018-01-17 11:33:32 -0700

    Played around with updating text files. No progress yey

--- src/existingOrNewDonor.js
+++ src/existingOrNewDonor.js
@@ -3,11 +3,25 @@ const path = require('path')
 const url = require('url')
 const fileExists = require('file-exists');
 
+$('document').ready(function() {
+  $('input').val('value')
+})
+
+
 function moveToDonorForm() {
 
 	document.getElementById(newdonor).innerHTML = window.location.replace("donorForm.html");
 }
 
+
+
+
+function urlExists(url) {
+		var http = new XMLHttpRequest();
+		http.open('HEAD', url, false);
+		http.send();
+		return http.status==200;
+	}
 
 
 function moveToExisistingForm() {
@@ -18,26 +32,35 @@ function moveToExisistingForm() {
 	var fileToSearch = searchedLastName + ", " + searchedFirstName + ".txt";
 	var searchedFile = "../donorFormEntries/" + fileToSearch;
 
-	function urlExists(url) {
-		var http = new XMLHttpRequest();
-		http.open('HEAD', url, false);
-		http.send();
-		return http.status==200;
-	}
 	if(urlExists(searchedFile)) {
 		var win = new BrowserWindow({ show: false, width: 500, height: 400});
 		win.loadURL(url.format({
-    	pathname: path.join(__dirname, searchedFile),
+    	pathname: path.join(__dirname, 'editDonorForm.html'),
     	protocol: 'file:',
     	slashes: true
   	}));
-
   //waiting to show the screen until now allows the screen and elements to load.
   	win.show();
 	}
 	else{
 		alert("File does not exist");
 	}
+}
+
+function filledForm() {
+	var arrayExample = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'];
+	document.getElementById(firstName).value = "a";
+	document.getElementById(lastName).value = arrayExample[1];
+	document.getElementById(contactName).value = arrayExample[2];
+	document.getElementById(emailAddress).value = arrayExample[3];
+	document.getElementById(phoneNumber).value = arrayExample[4];
+	document.getElementById(donationDate).value = arrayExample[5];
+	document.getElementById(address).value = arrayExample[6];
+	document.getElementById(city).value = arrayExample[7];
+	document.getElementById(postalCode).value = arrayExample[8];
+	document.getElementById(monetaryAmount).value = arrayExample[9];
+	document.getElementById(nonMonetaryItem).value = arrayExample[10];
+	document.getElementById(nonMonetaryAmount).value = arrayExample[11];
 }
 
 	//document.getElementById(newdonor).innerHTML = window.location.replace("donorFormV2.html");
