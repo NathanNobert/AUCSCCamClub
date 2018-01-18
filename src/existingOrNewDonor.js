330d27b5d9b020817114cec43b6a6b88fa42dbe8
JackShea1998 <jshea@ualberta.ca>
2018-01-17 20:11:04 -0700

    Changed the individual search from to boxes to one box so the predictive text will work better

--- src/existingOrNewDonor.js
+++ src/existingOrNewDonor.js
@@ -20,19 +20,30 @@ function urlExists(url) {
 		return http.status==200;
 	}
 
-
 function moveToExisistingForm() {
 	const remote = require('electron').remote;
 	const BrowserWindow = remote.BrowserWindow;
-	var searchedFirstName = document.getElementById("firstname").value;
-	var searchedLastName = document.getElementById("lastname").value;
+	var searchedFullName = document.getElementById("firstname").value;
+	var i = 0;
+	var searchedFirstName = "";
+
+	while(searchedFullName.charAt(i) != " "){
+		searchedFirstName = searchedFirstName + searchedFullName.charAt(i);
+		i = i + 1;
+	}
+
+	var searchedLastName = "";
+
+	for(counter = i + 1; counter < searchedFullName.length; counter++){
+		searchedLastName = searchedLastName + searchedFullName.charAt(counter);
+	}
 	var fileToSearch = searchedLastName + ", " + searchedFirstName + ".txt";
 	var searchedFile = "../donorFormEntries/" + fileToSearch;
 
 	if(urlExists(searchedFile)) {
 		var win = new BrowserWindow({ show: false, width: 500, height: 400});
 		win.loadURL(url.format({
-    	pathname: path.join(__dirname, 'editDonorForm.html'),
+    	pathname: path.join(__dirname, searchedFile),
     	protocol: 'file:',
     	slashes: true
   	}));
@@ -45,7 +56,20 @@ function moveToExisistingForm() {
 }
 
 function filledForm() {
-	var arrayExample = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'];
+	var fName = ['Timmy', '', '', '', '',];
+		window.onload=function(){
+		document.getElementById('firstName').value=fName[0];
+		document.getElementById('lastName').value='Last Name';
+		document.getElementById('contactName').value='Firsty';
+		document.getElementById('emailAddress').value='M@gmail.R';
+		document.getElementById('phoneNumber').value='1234567890';
+		document.getElementById('donationDate').value='1990-09-09';
+		document.getElementById('address').value='123 Fake Street';
+		document.getElementById('city').value='Fake City';
+		document.getElementById('postalCode').value='FFF FFF';
+		document.getElementById('monetaryAmount').value='2000';
+		document.getElementById('nonMonetaryItem').value='Bench';
+		document.getElementById('nonMonetaryAmount').value='200';}
 }
 
 	//document.getElementById(newdonor).innerHTML = window.location.replace("donorFormV2.html");
