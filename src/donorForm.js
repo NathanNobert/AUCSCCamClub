71b17a09c7bbc8e1b0932b79e817924e976b1b50
fbinsuma <fbinsuma@ualberta.ca>
2018-01-18 10:52:11 -0700

    Add quit/back buttons + implementation

--- src/donorForm.js
+++ src/donorForm.js
@@ -5,14 +5,13 @@ This file contains all the javascript used for the donorForm page.
 */
 
 const{app, BrowserWindow} = require('electron')
-
 const path = require ('path')
-
 const url = require ('url')
 const fs = require('fs')
 const PDFDocument = require('pdfkit')
+const main = remote.require('./index.js')
 
-const swal = require('sweetalert2')
+
 
 
 /*
@@ -23,6 +22,7 @@ This function reads the information from the donorForm.html file and creates a r
 function infoStorage() {
 
 	//var fileName = askWhereToSave();
+
 
 	var donorFormInfo = [{
 		first: document.getElementById("firstName").value,
@@ -42,11 +42,12 @@ function infoStorage() {
 		comment: document.getElementById("commentBox").value
 	}];//donorFormInfo
 
+
+	alert("please work")
+
 	var fs = require('fs');
-	var stream = fs.createWriteStream("donorFormEntries/" + donorFormInfo[0].last + ", " + donorFormInfo[0].first + ".txt"/*, {'flags':'a'}*/);
-
+	var stream = fs.createWriteStream("donorFormEntries/" + donorFormInfo[0].last + ", " + donorFormInfo[0].first + ".txt");
 	stream.once('open', function(fd) {
-
 	stream.write("Full Name: " + donorFormInfo[0].first + " " + donorFormInfo[0].last + "\r\n");
 	stream.write("Contact Name: " + donorFormInfo[0].contact + "\r\n");
 	stream.write("Email Address: " + donorFormInfo[0].email + "\r\n");
@@ -58,6 +59,8 @@ function infoStorage() {
 	stream.write("Comments: " + donorFormInfo[0].comment);
 	stream.end();
 	});
+
+
 
 	swal(
 		'Thank you for your submission',
@@ -92,157 +95,22 @@ function askWhereToSave(){
 
 }//askWhereToSave
 
-/*
-This function just sends the user back to the main menu without confirmation
-This is intentional because after submitting a form, just send the user back
 
-*/
-
-function gotoMainMenu() {
-  document.getElementById(gotoMainMenu).innerHTML = window.location.replace("selectPersonType.html");
-}//gotoMainMenu
-
-/*
-This function checkmarks the "check if given a receipt" checkbox when the "Print Receipt" button is pressed.
-Then it calls function createReceipt() which will create the pdf with the receipt for the donation.
-*/
 function printReceipt() {
-	document.getElementById("givenReceipt").checked = true;
-	createReceipt();
+	//Save the variables in the form that you need to print the receipt. 
+	let clientName = document.getElementById("firstName").value +" "+ document.getElementById("lastName").value;
+	let amountOfReceipt = document.getElementById("monetaryAmount").value + document.getElementById("nonMonetaryAmount").value;
+	let dateOfDonation = document.getElementById("donationDate").value;
+	alert("Name of client " + clientName +" Amount donated: "+amountOfReceipt+ " Date donated: "+dateOfDonation);
 
+	document.getElementById(receipt).innerHTML = window.location.replace("printDonationReceipt.html");
+	//Open a new Window
 }//printReceipt
 
-/*
-This function takes the information from the fields in the form that is needed to create the receipt. Then
-creates a new pdf and writes the information for the receipt in a logical organization in the pdf. So, in 
-the end you have a nice looking receipt.
-*/
-function createReceipt(){
-	//Saving the information needed for the receipt.
-	var receiptNumber = 1;
-
-	var donorFormInfo = [{
-		first: document.getElementById("firstName").value,
-		last: document.getElementById("lastName").value,
-		phone: document.getElementById("phoneNumber").value,
-		addressInfo: document.getElementById("address").value,
-		dateOfDonation: document.getElementById("donationDate").value,
-		cityInfo: document.getElementById("city").value,
-		provinceInfo: document.getElementById("province").value,
-		postal: document.getElementById("postalCode").value,
-		monetary: document.getElementById("monetaryAmount").value,
-		nonMonetary: document.getElementById("nonMonetaryAmount").value,
-		chequeInfo: document.getElementById("cheque").value,
-		cashInfo: document.getElementById("cash").value,
-		item: document.getElementById("nonMonetaryItem").value
-	}];//donorFormInfo
-
-	//Create a document
-	doc = new PDFDocument
-
-	//This adds in the basic template of the receipt.
-	doc.image('assets/images/receiptTemplate.png', {
-		fit: [500, 300],
-	});
-
-	//This writes the information that changes in the receipt to the pdf at exact locations.
-	doc.fontSize(12)
-	doc.text(receiptNumber, 480, 130);
-	doc.text(donorFormInfo[0].dateOfDonation, 180, 150);
-	doc.text(donorFormInfo[0].dateOfDonation, 390, 150);
-	doc.text(donorFormInfo[0].first + " " + donorFormInfo[0].last, 370, 166);
-	doc.text(donorFormInfo[0].addressInfo, 375, 185);
-	doc.text(donorFormInfo[0].cityInfo, 355, 200);
-	doc.text(donorFormInfo[0].provinceInfo, 460, 200);
-	doc.text(donorFormInfo[0].postal, 385, 220);
-	doc.text(donorFormInfo[0].phone, 460, 220);
-	doc.text(donorFormInfo[0].monetary, 200, 185);
-	doc.text(donorFormInfo[0].nonMonetary, 200, 200);
-
-	//Updates the receipt number
-	receiptNumber = receiptNumber + 1;
-
-	//Puts in the checkmark in the appropriate place in the receipt for the type of donation that was given.
-	if(document.getElementById("monetaryAmount").value != "" && document.getElementById("monetaryAmount").value != null){
-		if(document.getElementById("cheque").checked){
-			//CheckMark for if it is paid in cheque
-			doc.moveTo(150, 185)                         
-			doc.lineTo(155, 190)                            
-			doc.lineTo(160, 180)                            
-			doc.stroke() 
-		}
-
-		if(document.getElementById("cash").checked){
-			//CheckMark for it it is paid in cash.
-			doc.moveTo(150, 195)                         
-			doc.lineTo(155, 200)                            
-			doc.lineTo(160, 190)                            
-			doc.stroke() 
-		}
-	}
-
-	if(document.getElementById("nonMonetaryAmount").value != "" && document.getElementById("nonMonetaryAmount").value != null){
-		//CheckMark for it the donation was an item.
-		doc.moveTo(150, 210)                         
-		doc.lineTo(155, 215)                            
-		doc.lineTo(160, 205)                            
-		doc.stroke() 
-	}
-
-	//This is where the pdf is saved and how it is named.
-	doc.pipe(fs.createWriteStream("donorFormEntries/" + donorFormInfo[0].last + ", " + donorFormInfo[0].first + donorFormInfo[0].dateOfDonation + ".pdf"));
-
-	//Finalize PDF file
-	doc.end()
-}//createReceipt()
-
-
-function createThankYouCard(){
-	document.getElementById("givenCard").checked = true;
-}//createThankYouCard
 
 
 /*
-This function gets called when the user clicks the go back button, confirming if they want to go back
-to the main menu.
-*/
-function goBackToMainMenu(){
-	swal({
-  title: 'Are you sure you want to go back?',
-  text: "You won't be able to save this!",
-  type: 'warning',
-  showCancelButton: true,
-  confirmButtonColor: '#3085d6',
-  cancelButtonColor: '#d33',
-  confirmButtonText: 'go back!',
-  cancelButtonText: 'cancel!',
-  confirmButtonClass: 'btn btn-success',
-  cancelButtonClass: 'btn btn-danger',
-  buttonsStyling: false,
-  reverseButtons: true
-}).then((result) => {
-  if (result.value) {
-    swal(
-      'Going back!',
-      'Nothing is saved.',
-      'success'
-    ) 
-    document.getElementById(gotoMainMenu).innerHTML = window.location.replace("selectPersonType.html");
-  // result.dismiss can be 'cancel', 'overlay',
-  // 'close', and 'timer'
-  } else if (result.dismiss === 'cancel') {
-    swal(
-      'Cancelled',
-      ' ',
-      'error'
-    )
-  }
-})
-
-}
-
-/*
-This function gets called when the user clicks the submit form button,
+This function gets called when the user clicks the generate-pdf button,
 This creates a pdf report 
 */
 function makePDF(donorFormInfo){
@@ -250,6 +118,7 @@ function makePDF(donorFormInfo){
 	doc = new PDFDocument
 
 	//doc.image('../assets/images/logoWithTextUnder.png', 320, 280, scale: 0.25);
+
 
 	doc.text("Full Name: " + donorFormInfo[0].first + " " + donorFormInfo[0].last);
 	doc.text("Contact Name: " + donorFormInfo[0].contact);
@@ -339,7 +208,7 @@ function thankYouClick(donorFormInfo) {
 		doc.text(" ");
 		doc.text(" ");
 		doc.text(" ");
-		doc.text("Dear " + donorFormInfo[0].first + " " + donorFormInfo[0].last + "");
+		doc.text("Dear" + donorFormInfo[0].first + ", " + donorFormInfo[0].last + "");
 		doc.text(" ");
 		doc.text(" ");
 		doc.text("On behalf of of Camrose Boys And Girls Club, I would like to thank you for your genrous donation on " + donorFormInfo[0].donationDate);
